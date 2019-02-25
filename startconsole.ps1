19a7e6e4c3cb769fd1c10c0748c1bb4d9c5d27d4
Toby Milne <tm@svector.co.uk>
2019-02-25 11:15:04 +0000

    Initial

---
+++ startconsole.ps1
@@ -0,0 +1,14 @@
+﻿param (
+    [string]$environment 
+    )
+#assumes that assume-role is installed and setup for the environment
+#https://github.com/remind101/assume-role
+#TLDR - 
+#iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+#scoop bucket add extras
+#scoop install assume-role
+
+assume-role.exe $environment | Invoke-Expression
+$loginUrl = & python $PSScriptRoot\signin.py
+
+Invoke-Expression "start '$loginUrl'"
