5670a298cc3c44bfb125cc438ece238805852a84
Toby Milne <tm@svector.co.uk>
2019-03-06 12:21:36 +0000

    Use new user data dir for each environment

--- vault/startconsole.ps1
+++ vault/startconsole.ps1
@@ -3,5 +3,15 @@
     )
 #assumes that aws-vault is installed and setup for the environment
 $loginUrl = aws-vault login $environment -s
+$profileFolder = "${env:LOCALAPPDATA}\ChromeProfile_${environment}"
 
-Invoke-Expression "start '$loginUrl'"
+$dir = Get-Item -Path $profileFolder
+
+if ($null -eq $dir)
+{
+	New-Item -ItemType directory -Path $profileFolder
+}
+
+$userDataDir = "--user-data-dir="+$profileFolder
+
+Start-Process chrome.exe -ArgumentList @( "-incognito", $userDataDir,$loginUrl )
